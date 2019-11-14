f8fcb3b5c9e1e79d945e9492727ebb3f0cd90910
ajchen42 <chen.ad@husky.neu.edu>
2019-11-14 17:58:20 -0500

    final changes

--- test/SpreadsheetTextualViewTest.java
+++ test/SpreadsheetTextualViewTest.java
@@ -29,9 +29,9 @@ public class SpreadsheetTextualViewTest {
   public void testFileIO() {
 
     // make a model from a file
-    BasicSpreadsheetModel model;
+    BasicSpreadsheetModel model1;
     try {
-      model = WorksheetReader
+      model1 = WorksheetReader
           .read(new WorkSheetBuilderImpl(), new FileReader("resources/input1.txt"));
     } catch (FileNotFoundException ex) {
       throw new IllegalStateException("file not found");
@@ -40,7 +40,7 @@ public class SpreadsheetTextualViewTest {
     //initialize a print writer to append to, and one to clear the file
     PrintWriter pw;
     PrintWriter clear;
-    File file = new File("resources/input1-test.txt");
+    File file = new File("resources/input1-textualview.txt");
 
     try {
       file.createNewFile();
@@ -58,7 +58,7 @@ public class SpreadsheetTextualViewTest {
     clear.print("");
     clear.close();
     // create a new view using the cleared new file and old file model
-    SpreadsheetView<Cell> textView = new SpreadsheetTextualView(pw, model);
+    SpreadsheetView<Cell> textView = new SpreadsheetTextualView(pw, model1);
 
     // save to the new file
     try {
@@ -79,7 +79,7 @@ public class SpreadsheetTextualViewTest {
     }
 
     // ensure the models are the same
-    Map<Coord, Cell> map1 = model.getAllCells();
+    Map<Coord, Cell> map1 = model1.getAllCells();
     Map<Coord, Cell> map2 = model2.getAllCells();
 
     for (Map.Entry<Coord,Cell> entry : map1.entrySet()) {
@@ -91,6 +91,96 @@ public class SpreadsheetTextualViewTest {
       assertTrue(map1.containsKey(entry.getKey()));
       assertEquals(map1.get(entry.getKey()), entry.getValue());
     }
+
+  }
+
+  /**
+   * Creating other textual views for all input examples.
+   */
+  @Test
+  public void createOtherTextViewFiles() {
+
+    // make a model from input2
+    BasicSpreadsheetModel model1;
+    try {
+      model1 = WorksheetReader
+          .read(new WorkSheetBuilderImpl(), new FileReader("resources/input2.txt"));
+    } catch (FileNotFoundException ex) {
+      throw new IllegalStateException("file not found");
+    }
+
+    //initialize a print writer to append to, and one to clear the file
+    PrintWriter pw;
+    PrintWriter clear;
+    File file = new File("resources/input2-textualview.txt");
+
+    try {
+      file.createNewFile();
+      pw = new PrintWriter(new FileOutputStream(file.getAbsoluteFile(),true));
+    } catch (Exception ex) {
+      throw new IllegalStateException("file not found");
+    }
+
+    try {
+      clear = new PrintWriter(file.getAbsoluteFile());
+    } catch (Exception ex) {
+      throw new IllegalStateException("file not found");
+    }
+    //clear the file
+    clear.print("");
+    clear.close();
+    // create a new view using the cleared new file and old file model
+    SpreadsheetView<Cell> textView = new SpreadsheetTextualView(pw, model1);
+
+    // save to the new file
+    try {
+      textView.render();
+    } catch (IOException ex) {
+      throw new IllegalStateException("IOException");
+    }
+
+    pw.close();
+
+    // make a model from a file
+    BasicSpreadsheetModel model2;
+    try {
+      model2 = WorksheetReader
+          .read(new WorkSheetBuilderImpl(), new FileReader("resources/input3.txt"));
+    } catch (FileNotFoundException ex) {
+      throw new IllegalStateException("file not found");
+    }
+
+    //initialize a print writer to append to, and one to clear the file
+    PrintWriter pw2;
+    PrintWriter clear2;
+    File file2 = new File("resources/input3-textualview.txt");
+
+    try {
+      file.createNewFile();
+      pw2 = new PrintWriter(new FileOutputStream(file2.getAbsoluteFile(),true));
+    } catch (Exception ex) {
+      throw new IllegalStateException("file not found");
+    }
+
+    try {
+      clear2 = new PrintWriter(file2.getAbsoluteFile());
+    } catch (Exception ex) {
+      throw new IllegalStateException("file not found");
+    }
+    //clear the file
+    clear2.print("");
+    clear2.close();
+    // create a new view using the cleared new file and old file model
+    SpreadsheetView<Cell> textView2 = new SpreadsheetTextualView(pw2, model2);
+
+    // save to the new file
+    try {
+      textView2.render();
+    } catch (IOException ex) {
+      throw new IllegalStateException("IOException");
+    }
+
+    pw2.close();
 
   }
 }
