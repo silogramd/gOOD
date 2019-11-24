36de18ce8b33eb999908781a8611cc04729e9e66
ajchen42 <chen.ad@husky.neu.edu>
2019-11-24 00:42:32 -0500

    fixing editable

--- src/edu/cs3500/spreadsheets/view/SpreadsheetEditableView.java
+++ src/edu/cs3500/spreadsheets/view/SpreadsheetEditableView.java
@@ -3,14 +3,17 @@ package edu.cs3500.spreadsheets.view;
 import edu.cs3500.spreadsheets.model.Cell;
 import edu.cs3500.spreadsheets.model.ReadOnlyModel;
 import edu.cs3500.spreadsheets.model.SpreadsheetModel;
+import java.awt.BorderLayout;
 import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class SpreadsheetEditableView extends JFrame implements SpreadsheetGUIView {
 
+  private static int WIDTH = 15;
+  private static int HEIGHT = 30;
   private final SpreadsheetModel<Cell> model;
-  private final SpreadsheetFrameView sheet;
+  private SpreadsheetGUIViewPanel panel;
   private final JTextField[][] textGrid;
   private ViewEventListener listener;
 
@@ -20,10 +23,22 @@ public class SpreadsheetEditableView extends JFrame implements SpreadsheetGUIVie
    * @param model to be displayed
    */
   public SpreadsheetEditableView(SpreadsheetModel<Cell> model, ViewEventListener v) {
+    super();
     this.listener = listener;
     this.model = model;
-    this.sheet = new SpreadsheetFrameView(new ReadOnlyModel(this.model));
-    this.textGrid = this.sheet.fieldGrid;
+
+    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    this.setLayout(new BorderLayout());
+
+    textGrid = new JTextField[HEIGHT][WIDTH];
+
+    panel = new SpreadsheetGUIViewPanel(model, WIDTH, HEIGHT);
+
+    this.add(panel, BorderLayout.CENTER);
+    this.pack();
+    this.setLocationByPlatform(true);
+
+
     addListeners();
   }
 
@@ -31,8 +46,8 @@ public class SpreadsheetEditableView extends JFrame implements SpreadsheetGUIVie
    * Adds the {@link ViewEventListener} as a listener for the textgrid.
    */
   private void addListeners() {
-    for (int i = 0; i < sheet.getHeight(); i++) {
-      for (int j = 0; j < sheet.getWidth(); j++) {
+    for (int i = 0; i < HEIGHT; i++) {
+      for (int j = 0; j < WIDTH; j++) {
         listener.addTextEvent(textGrid[i][j]);
       }
     }
@@ -41,14 +56,17 @@ public class SpreadsheetEditableView extends JFrame implements SpreadsheetGUIVie
 
   @Override
   public void render() throws IOException {
-    
+    this.setVisible(true);
   }
 
   @Override
   public void refresh() throws IOException {
-
+    this.panel.refresh();
+    this.pack();
+    this.repaint();
   }
 
+  /*
   @Override
   public int getHeight() {
     return sheet.getHeight();
@@ -58,4 +76,5 @@ public class SpreadsheetEditableView extends JFrame implements SpreadsheetGUIVie
   public int getWidth() {
     return sheet.getWidth();
   }
+  */
 }
